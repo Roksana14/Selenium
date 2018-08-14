5ab0574a9ea5630d93a99b9591c7c10e3533253f
Roksana <roksana@1450.pl>
2018-08-14 17:38:23 +0200

    interaktywność

--- ProjektKonsola/ProjektKonsola/ConsoleApp1/Program.cs
+++ ProjektKonsola/ProjektKonsola/ConsoleApp1/Program.cs
@@ -8,34 +8,72 @@ namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            int kwota = 1;
-            Skarbonka mojaSkarbonka = new Skarbonka();
-            while (mojaSkarbonka.ZawartoscSkarbonki <= 20)
-            {
-                Console.WriteLine("Podaj kwotę jaką chcesz włożyć do skarbonki");
-                kwota = int.Parse(Console.ReadLine());
-                mojaSkarbonka.WlozPieniadze(kwota);
-                Console.WriteLine("Twoja zawartość skarbonki to:");
-                Console.WriteLine(mojaSkarbonka.ZawartoscSkarbonki);
-                Console.WriteLine("Podaj kwotę jaką chcesz wyłożyć ze skarbonki");
-                kwota = int.Parse(Console.ReadLine());
-                if(mojaSkarbonka.ZawartoscSkarbonki > kwota)
-                {
-                    mojaSkarbonka.WyjmijPieniadze(kwota);
-                }
-                else
-                {
-                    Console.WriteLine("Nie możesz wybrać tylu piniądzów");
-                }
-                Console.WriteLine("Twoja zawartość skarbonki to:");
-                Console.WriteLine(mojaSkarbonka.ZawartoscSkarbonki);
-            }
-            Console.ReadLine();
+		static void Main(string[] args)
+		{
+			int kwota = 1;
+			string akcja = string.Empty;
+			Skarbonka mojaSkarbonka = new Skarbonka();
+			mojaSkarbonka.WyswietlDostepneAkcje();
+			while (akcja != "4")
+			{
+				akcja = Console.ReadLine();
+				switch (akcja)
+				{
+					case "1": Console.WriteLine("Twoja zawartość skarbonki to:");
+						      Console.WriteLine(mojaSkarbonka.ZawartoscSkarbonki);
+						break;
+					case "2": Console.WriteLine("Podaj kwotę jaką chcesz włożyć do skarbonki");
+						      kwota = int.Parse(Console.ReadLine());
+						      mojaSkarbonka.WlozPieniadze(kwota);
+						break;
+					case "3": Console.WriteLine("Podaj kwotę jaką chcesz wyłożyć ze skarbonki");
+						      kwota = int.Parse(Console.ReadLine());
+						break;
+
+					case "5": mojaSkarbonka.WyswietlDostepneAkcje();
+						break;
+					case "4":
+						break;
+					default:Console.WriteLine(" Twoja akcja jest nieprawidłowa");
+						    mojaSkarbonka.WyswietlDostepneAkcje();
+						break;
+				}
+
+				//if (akcja == "1")
+				//{
+				//	Console.WriteLine("Twoja zawartość skarbonki to:");
+				//	Console.WriteLine(mojaSkarbonka.ZawartoscSkarbonki);
+				//}
+				//else if (akcja == "2")
+				//{
+				//	Console.WriteLine("Podaj kwotę jaką chcesz włożyć do skarbonki");
+				//	kwota = int.Parse(Console.ReadLine());
+				//	mojaSkarbonka.WlozPieniadze(kwota);
+				//}
+				//else if (akcja == "3")
+				//{
+				//	Console.WriteLine("Podaj kwotę jaką chcesz wyłożyć ze skarbonki");
+				//	kwota = int.Parse(Console.ReadLine());
+
+				//	if (mojaSkarbonka.ZawartoscSkarbonki > kwota)
+				//	{
+				//		mojaSkarbonka.WyjmijPieniadze(kwota);
+				//	}
+				//	else
+				//	{
+
+				//		Console.WriteLine("Nie możesz wybrać tylu piniądzów");
+				//	}
+				//}
+				//else if (akcja == "5")
+				//{
+				//	mojaSkarbonka.WyswietlDostepneAkcje();
+				//}
+
+			}
 
 
-        }
+		}
         class Skarbonka
         {
             private int zawartoscSkarbonki = 0;
@@ -51,6 +89,16 @@ namespace ConsoleApp1
             {
                 zawartoscSkarbonki = ZawartoscSkarbonki - piniadz;
             }
+
+			public void WyswietlDostepneAkcje()
+			{
+				Console.WriteLine("1-Wyświetl zawartość skarbonki");
+				Console.WriteLine("2-Włóż pieniądz");
+				Console.WriteLine("3-Wyłóż pieniądze");
+				Console.WriteLine("4-Zakończ program");
+				Console.WriteLine("5-Wyświetl dostępne akcje");
+			}
+
         }
     }
 }
